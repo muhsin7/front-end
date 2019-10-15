7432084ade1e46ea01da4d2e477ff7329f3d2f30
muhsin <35616096+muhsin7@users.noreply.github.com>
2019-10-15 21:25:31 +0530

    added test + format files
    tried adding materialize dropdown, worked in test.html, but not in assignments.html; gave up to study lol

--- front end/js/initializing.js
+++ front end/js/initializing.js
@@ -11,3 +11,15 @@ $(document).ready(function(){
   $('.modal').modal();
 });
 ///////////////////
+
+
+//////DROPDOWN//////
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Dropdown.init(elems, options);
+});
+
+// Or with jQuery
+
+$('.dropdown-trigger').dropdown();
+///////////////////
