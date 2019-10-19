ed83972983915f2699750173aac257a8f4a94128
muhsin <35616096+muhsin7@users.noreply.github.com>
2019-10-19 21:09:22 +0530

    bunch of shit

--- front end/js/initializing.js
+++ front end/js/initializing.js
@@ -18,8 +18,12 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.dropdown-trigger');
   var instances = M.Dropdown.init(elems, options);
 });
-
-// Or with jQuery
-
-$('.dropdown-trigger').dropdown();
 ///////////////////
+
+
+//////FLOATING BUTTON//////
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, options);
+  });
+//////////////////////////
