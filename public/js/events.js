4e3507a4708e4c4f7ff424669504925fcdafc393
BuildTools <kabirdesarkar@gmail.com>
2019-10-24 21:02:53 +0530

    Extended forms(incomplete)
    Migration from old code to new is going well

--- public/js/events.js
+++ public/js/events.js
@@ -1,6 +1,8 @@
 var l = console.log;
 $.initSession = () => {
-  l("Hello World!");
+  $(() => {
+    $(window).trigger("sessionLoaded",["Hello sir"])
+  })
 }
 $(() => {
   $.fn.arr = function(){
@@ -15,9 +17,11 @@ $(() => {
       form.trigger('submit')
     }
   })
-  $('.form button[type=submit]').on('click',function(e){
-    let form = $(this).parents('.form').first()
-    form.trigger('submit')
+  $('.form [type=submit]').on('click',function(e){
+    // l($(this).parents('.form'))
+    form = $(this).parents('.form').first()
+    if (typeof form.attr("indirect-submit") != "undefined" && form.attr("indirect-submit") == 'true') form.trigger("confirm");
+    else form.trigger("submit")
   })
   $('.form').on('submit',function(e){
     // let form = $(this).parents('.form').first();
@@ -28,19 +32,23 @@ $(() => {
         data = new FormData();
         // console.log("Yes this is formdata type")
         for(let elem of form.find('input')){
-          if($(elem).attr('type') == 'file'){
-            // console.log('yes this type file')
-            data.append($(elem).attr("name"),new Blob([elem.files[0]],{type:'image/jpeg'}))
-          } else {
-            data.append($(elem).attr("name"),$(elem).val())
+          elem = $(elem)
+          if (typeof elem.attr("ignore-value") == "undefined" &&  elem.parents("[ignore-valu-container]").length==0) {
+              if(elem.attr('type') == 'file'){
+                // console.log('yes this type file')
+                data.append($(elem).attr("name"),new Blob([elem.files[0]],{type:'image/jpeg'}))
+              } else {
+                data.append($(elem).attr("name"),$(elem).val())
+              }
           }
         }
       } else {
         data = {}
-        for(let elem of form.find('input')){
-          // l(elem)
+        for(let elem of form.find('input[name],select[name]')){
           elem = $(elem);
-          data[elem.attr('name')] = elem.val();
+          if((typeof elem.attr("ignore-value") == "undefined" && elem.parents('[ignore-value-container]').length == 0)||(typeof elem.attr("read-value") != "undefined" || elem.parents("[read-value-container]").length != 0)) {
+            data[elem.attr('name')] = elem.val();
+          }
         }
       }
     }
@@ -86,6 +94,6 @@ $(() => {
     if(form.attr("on-error-prompt-selector") != undefined){
       form.find(form.attr("on-error-prompt-selector")).html(response)
     }
-    // l(response)
+    l(response)
   })
 })
