b2414d8cb94217a4652dda7c7fdac240af6e6606
muhsin <35616096+muhsin7@users.noreply.github.com>
2019-11-03 19:14:22 +0530

    linking hrefs with json or something (linking links?)

--- public/private/easy_element_generator.py
+++ public/private/easy_element_generator.py
@@ -3,7 +3,7 @@
 a = "Chemistry HL, Physics HL, Mathematics AA HL, Spanish Ab HL, Language and Literature SL, Economics SL"
 a = a.split(', ')
 for x in a:
-    print(f'<a href="class.html"><li class="truncate">{x}</li></a>')
+    print(f'<a href="/class/"><li class="truncate">{x}</li></a>')
 
 n = 1
 for i in a:
