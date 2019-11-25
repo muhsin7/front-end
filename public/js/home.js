74a67489bbd4fbabb63aae450d546210bc7e11fa
muhsin7 <muhsin.ma.mohamed@gmail.com>
2019-11-25 09:47:03 +0530

    post revamp with comments
    - changed styles + format on posts
    - edited home.js template
    - added edit, delete modals
    - added "read more" js to script.js
    - added post to view-map.json
    **notes for kabir**
    - if the account is a teacher, add ```html
    <a class="tooltipped" data-posiion="left" data-tooltip="Teacher"><i class="material-icons nord-yellow-text">school</i></a>``` at the ${ifTeacherAccountAddElementHere} variable
    - when adding post content do this: ```js
    $('#textarea1').val('New Text');
      M.textareaAutoResize($('#textarea1'));
    ```

--- public/js/home.js
+++ public/js/home.js
@@ -55,6 +55,53 @@ $(() => {
     success:function(response){
       // console.log(response)
       let postContainer = $('#postContainer')
+      // for(let post of response){
+      //   let {classID,className,content,title,posterName,dateCreated} = post;
+      //   dateCreated = new Date(dateCreated).getSemiSimpleTime();
+      //
+      //   let elem = $(`<div class="post">
+      //     <a href="/class/${classID}" class="assignment-subject bold-text blue-text">${className}</a>
+      //     <h5>${title}</h5>
+      //     <h7>${posterName}</h7><br>
+      //     <span class="grey-text small-text">${dateCreated}</span>
+      //     <h6>${content}</h6>
+      //   </div>`)
+      //   postContainer.append(elem)
+      // }
+      for(let post of response){
+        let {classID,className,content,title,posterName,dateCreated} = post;
+        dateCreated = new Date(dateCreated).getSemiSimpleTime();
+
+        let elem = $(`<div class="post hoverable" id="samplePost">
+          <div class="post-header">
+            <img class="post-pfp" src="../images/default.jpg" />
+          </div>
+          <div class="poster">${posterName}<a href="#" class="dropdown-trigger" data-target="post-dropdown"><i class="material-icons post-options">more_horiz</i></a></div>
+          ${ifTeacherAccountAddElementHere}
+          <a href="/class/${classID}" class="post-class">${className}</a>
+          <span class="date-text">${dateCreated}</span>
+          <div class="more">${content}</div>
+          <hr />
+          <h6><b>Comments</b></h6>
+          <div class="form">
+            <input name="comment" type="text" class="comment-input"/>
+            <button name="comment-btn" type="submit" name="button" class="right-align btn nord-red">Comment</button>
+            <br /><br />
+          </div>
+          <div class="comments">
+            <div class="comment">
+              <div class="comment-marker nord-darkblue"></div>
+              <img class="comment-pfp" src="../images/default.jpg"/>
+              <div class="comment-poster">Kabir</div><span class="date-text">${dateCommented}</span>
+              <div class="comment-text more">$commentContent</div>
+            </div>
+          </div>
+          <div class="load-more-comments">
+            <a href="/post/${postID}">[Load more comments...]</a>
+          </div>
+        </div>`)
+        postContainer.append(elem)
+      }
       for(let post of response){
         let {classID,className,content,title,posterName,dateCreated} = post;
         dateCreated = new Date(dateCreated).getSemiSimpleTime();
